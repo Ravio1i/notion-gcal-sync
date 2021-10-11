ffb112d2f82f07b0e49ff7cbf12ba152e7a401c5
Luka Kroeger <luka.kroeger@gmail.com>
2021-10-11 22:05:51 +0200

    Refactor and add tests

---
+++ tests/events/test_GCalEvent.py
@@ -0,0 +1,22 @@
+def test_from_api():
+    assert False
+
+
+def test_get_meta():
+    assert False
+
+
+def test_get_time():
+    assert False
+
+
+def test_get_calendar():
+    assert False
+
+
+def test_get_recurrent_event():
+    assert False
+
+
+def test_body():
+    assert False
---
+++ tests/test_Config.py
@@ -0,0 +1,30 @@
+def test_default_event_length():
+    assert False
+
+
+def test_default_event_length():
+    assert False
+
+
+def test_no_date_action():
+    assert False
+
+
+def test_no_date_action():
+    assert False
+
+
+def test_get_calendar_id():
+    assert False
+
+
+def test_get_calendar_name():
+    assert False
+
+
+def test_is_valid_calendar_name():
+    assert False
+
+
+def test_is_valid_calendar_id():
+    assert False
